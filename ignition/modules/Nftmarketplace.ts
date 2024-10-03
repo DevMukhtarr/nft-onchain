554e2b5c9893091ba6f8c5675b098c9e2f30d1e5
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-10-03 14:14:25 +0100

    nft marketplace contract where trade happens

--- ignition/modules/Nftmarketplace.ts
+++ ignition/modules/Nftmarketplace.ts
@@ -3,7 +3,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const nftAddress = "0xB4f562a85b7F9cCF9F8bC0fdB708eC8F7a759381";
 const NftmarketplaceModule = buildModule("NftmarketplaceModule", (m) => {
 
-  const nftmarketplace = m.contract("Nftmarketplace", [nftAddress]);
+  const nftmarketplace = m.contract("NftMarketplace", [nftAddress]);
 
   return { nftmarketplace };
 });
